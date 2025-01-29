c0e0aad024aaf86499a0e14420f71a74faea66cf
keggre <kegocandy@gmai.com>
2025-01-29 18:16:25 -0500

    Laying the groundwork.

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
+def index():
+	if False:
+		return redirect("/login")
+	else:
+		return render_template("app.html")
+
+
+@app.route("/login")
 def login():
 	return render_template("login.html")
+
+
+@app.route("/api/login", methods=["POST"])
+def api_login():
+	return
 
 
 if __name__ == "__main__":
