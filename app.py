ef6f155a0cc01a5d5d9f4f68ef6a90b395fc2497
keggre <kegocandy@gmai.com>
2025-02-05 16:38:06 -0500

    Actually that was a bad idea.

--- app.py
+++ app.py
@@ -540,8 +540,8 @@ def letter_generate():
 		examples += [(resume, "No job description provided.", sample)]
 	if os.path.exists(save_path):
 		for directory in os.listdir(save_path):
-			if read_user_file(user_id, "letter.md") != read_user_file(user_id, os.path.join(save_path, directory, "letter.md")): # This way a cover letter for the same job only loads as an example if the user modified it
-				examples += [load(os.path.join(save_path, directory))]
+			# if read_user_file(user_id, "letter.md") != read_user_file(user_id, os.path.join(save_path, directory, "letter.md")): # This way a cover letter for the same job only loads as an example if the user modified it
+			examples += [load(os.path.join(save_path, directory))]
 	# print(examples)
 	
 	# Use Ollama to generate the job title and cover letter
